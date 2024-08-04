8f90f5fc9ddb6c59003cfbddd2ce58cd17802a9e
Jonas <jonascastro.dev@gmail.com>
2024-08-04 13:11:41 -0300

    Configurações iniciais do projeto

---
+++ src/test/java/com/example/orange/OrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
